7bcf47dd2ce8d389a6ae73f50896b2874e1f297a
Ben Mather <bwhmather@gmail.com>
2013-09-24 22:14:06 +0100

    add decorator for catching glfw errors

--- glfw/error.py
+++ glfw/error.py
@@ -1,6 +1,14 @@
 from glfw.constants import ErrorCode
 from glfw._glfw import ffi, libglfw
 
+import functools
+
+
+class PriorErrorError(Exception):
+    """ An error occured a while ago but nobody bothered to check
+    """
+    pass
+
 
 class NotInitialised(Exception):
     pass
@@ -67,3 +75,27 @@ def check_error():
     error, _current_error = _current_error, None
     if error:
         raise error
+
+
+def throw_errors(f):
+    """ Decorate a function calling a glfw native function so that glfw errors
+    are raised as exceptions.
+    """
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        global _current_error
+        assert not _current_error
+
+        error = None
+
+        try:
+            r = f(*args, **kwargs)
+        finally:
+            # always clear the error if the function raised something
+            error, _current_error = _current_error, None
+
+        if error:
+            raise error
+
+        return r
+    return wrapper
