7414300937c9f689867aa035d871bcc64465e9f8
Ben Mather <bwhmather@gmail.com>
2013-09-24 21:37:51 +0100

    docs

--- glfw/__init__.py
+++ glfw/__init__.py
@@ -27,21 +27,46 @@ def get_version(verbose=False):
     else:
         return str(libglfw.glfwGetVersionString())
 
+
 #GLFWerrorfun glfwSetErrorCallback(GLFWerrorfun cbfun);
 # TODO
 #void glfwPollEvents(void);
 def poll_events():
+    """ Process events that have already been received and return immediately.
+
+    This will invoke all relevant window callbacks in the current thread.
+
+    This function is not required for joystick input to work.
+
+    .. note::
+      - This function may only be called from the main thread.
+      - This function may not be called from a callback.
+      - On some platforms, certain callbacks may be called outside of a call
+        to one of the event processing functions.
+
+    .. seealso::
+      :py:fun:`wait_events`
+
+    """
     libglfw.glfwPollEvents()
+
 
 #void glfwWaitEvents(void);
 def wait_events():
+    """ Sleep until at least one event has been received.
+
+    This will invoke all relevant window callbacks in the current thread.
+
+    This function is not required for joystick input to work.
+
+    .. note::
+      - This function may only be called from the main thread.
+      - This function may not be called from a callback.
+      - On some platforms, certain callbacks may be called outside of a call
+        to one of the event processing functions.
+
+    .. seealso::
+        :py:fun:`poll_events`
+
+    """
     libglfw.glfwWaitEvents()
-
-
-#void glfwSetClipboardString(GLFWwindow* window, const char* string);
-#const char* glfwGetClipboardString(GLFWwindow* window);
-#double glfwGetTime(void);
-#void glfwSetTime(double time);
-#void glfwSwapInterval(int interval);
-#int glfwExtensionSupported(const char* extension);
-#GLFWglproc glfwGetProcAddress(const char* procname);
